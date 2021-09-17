088438ff378c3710d334a1cbfbc09e4442aeaa23
kevinbasoftcat <kevinba@softcat.com>
2021-09-17 10:58:20 +0100

    Variable update

--- Azure/Update-Management/variables.tf
+++ Azure/Update-Management/variables.tf
@@ -1,9 +1,7 @@
 variable "rgname" {
     type = string
-    description = "Resource group name to deploy the Update Management resources into"
-  
+    description = "Resource group name to deploy the Update Management resources into" 
 }
-
 variable "resourceprefix" {
     type = string
     description = "Prefix to use when naming each of the resources please ensure this is only 3 characters long"
