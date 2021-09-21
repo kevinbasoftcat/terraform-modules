0b57abd932f752aee1960c0bf98bc23d98e8a9cb
kevinbasoftcat <kevinba@softcat.com>
2021-09-21 08:57:49 +0100

    Variable update

--- Azure/Network/variables.tf
+++ Azure/Network/variables.tf
@@ -2,6 +2,14 @@ variable "rgname" {
     type = string
     description = "Resource group name to deploy the Update Management resources into" 
 }
+variable "resourceprefix" {
+    type = string
+    description = "Prefix to use when naming each of the resources please ensure this is only 3 characters long"
+    validation {
+      condition = length (var.resourceprefix) == 3
+      error_message = "The resource prefix must be only 3 characters long."
+    }
+}
 variable "vnetaddressprefix" {
     type = string
     description = "Please enter a valid network CIDR block to use for the vNet. This must be a CIDR notation of /24 or larger"
