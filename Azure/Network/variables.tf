4c54a66d98e3ccab454189a61100f1ce679fd913
kevinbasoftcat <kevinba@softcat.com>
2021-09-21 15:34:04 +0100

    Updated variables

--- Azure/Network/variables.tf
+++ Azure/Network/variables.tf
@@ -31,7 +31,7 @@ variable "bastionnsgrules" {
         access                      = string
         protocol                    = string
         source_port_range           = string
-        destination_port_range      = string
+        destination_port_range      = list(string)
         source_address_prefix       = string
         destination_address_prefix  = string
     }))
