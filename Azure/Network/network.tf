705584766a65798bfd30002a191143ea58f7df46
kevinbasoftcat <kevinba@softcat.com>
2021-09-21 11:11:57 +0100

    Adding subnets

--- Azure/Network/network.tf
+++ Azure/Network/network.tf
@@ -3,3 +3,28 @@ provider "azurerm" {
   }
 }
 
+resource "random_string" "randomstring" {
+  length = 5
+  special = false
+  lower = true
+  number = true
+}
+
+resource "azurerm_resource_group" "vNet" {
+  name = var.rgname
+  location = "UK South"
+}
+
+resource "azurerm_virtual_network" "vNet" {
+  name = "${var.resourceprefix}-vNet-${random_string.randomstring.result}"
+  location = azurerm_resource_group.vNet.location
+  resource_group_name = azurerm_resource_group.vNet.name
+  address_space = var.vnetaddressprefix
+}
+
+resource "azurerm_sunet" "vNetBastionSubnet" {
+  name = "AzureBastionSubnet"
+  resource_group_name = azurerm_resource_group.vNet.Name
+  virtual_network_name = azurerm_virtual_network.vNet.name
+  address_prefixes = var.bastionhostsubnet
+}
