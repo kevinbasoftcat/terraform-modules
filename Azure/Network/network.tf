bae812bcec357ae4791138ba630c5c3007b7c01f
kevinbasoftcat <kevinba@softcat.com>
2021-09-21 13:42:15 +0100

    added monitoring subnet

--- Azure/Network/network.tf
+++ Azure/Network/network.tf
@@ -28,3 +28,11 @@ resource "azurerm_subnet" "vNetBastionSubnet" {
   virtual_network_name = azurerm_virtual_network.vNet.name
   address_prefixes = ["${var.bastionhostsubnet}"]
 }
+
+resource "azurerm_subnet" "vNetMonitoringSubnet" {
+    name = "AzureMonitoringSubnet"
+    resource_group_name = azurerm_resource_group.vNet.name
+    virtual_network_name = azurerm_virtual_network.vNet.name
+    address_prefixes = ["${var.monitoringsubnet}"]
+  
+}
