fdaf8b30250f1fc45693e44f399ee7022124765f
kevinbasoftcat <kevinba@softcat.com>
2021-09-22 15:24:28 +0100

    Updated template layout

--- Azure/Network/network.tf
+++ Azure/Network/network.tf
@@ -2,40 +2,35 @@ provider "azurerm" {
   features { 
   }
 }
-
 resource "random_string" "randomstring" {
   length = 5
   special = false
   lower = true
   number = true
 }
-
 resource "azurerm_resource_group" "vNet" {
   name = var.rgname
   location = "UK South"
 }
-
 resource "azurerm_virtual_network" "vNet" {
   name = "${var.resourceprefix}-vNet-${random_string.randomstring.result}"
   location = azurerm_resource_group.vNet.location
   resource_group_name = azurerm_resource_group.vNet.name
   address_space = ["${var.vnetaddressprefix}"]
-}
 
+}
 resource "azurerm_subnet" "vNetBastionSubnet" {
   name = "AzureBastionSubnet"
   resource_group_name = azurerm_resource_group.vNet.name
   virtual_network_name = azurerm_virtual_network.vNet.name
   address_prefixes = ["${var.bastionhostsubnet}"]
 }
-
 resource "azurerm_subnet" "vNetMonitoringSubnet" {
     name = "AzureMonitoringSubnet"
     resource_group_name = azurerm_resource_group.vNet.name
     virtual_network_name = azurerm_virtual_network.vNet.name
     address_prefixes = ["${var.monitoringsubnet}"]
 }
-
 resource "azurerm_network_security_group" "bastionNSG" {
     name = "${var.resourceprefix}-BastionNSG-${random_string.randomstring.result}"
     location = azurerm_resource_group.vNet.location
@@ -66,7 +61,6 @@ resource "azurerm_network_security_group" "bastionNSG" {
         }
     }
 }
-
 resource "azurerm_subnet_network_security_group_association" "bastionNSG" {
     subnet_id = azurerm_subnet.vNetBastionSubnet.id
     network_security_group_id = azurerm_network_security_group.bastionNSG.id
