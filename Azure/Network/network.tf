9de697a0f8055910cc867daadd6293c43e5a02d0
kevinbasoftcat <kevinba@softcat.com>
2021-09-28 09:16:58 +0100

    network change

--- Azure/Network/network.tf
+++ Azure/Network/network.tf
@@ -22,8 +22,8 @@ resource "azurerm_virtual_network" "vNet" {
 resource "azurerm_subnet" "subnet" {
   count = length(var.subnet_names)
   name                                           = var.subnet_names[count.index]
-  resource_group_name                            = data.azurerm_resource_group.vnet.name
-  virtual_network_name                           = azurerm_virtual_network.vnet.name
+  resource_group_name                            = azurerm_resource_group.vNet.name
+  virtual_network_name                           = azurerm_virtual_network.vNet.name
   address_prefixes                               = [var.subnet_prefixes[count.index]]
   service_endpoints                              = lookup(var.subnet_service_endpoints, var.subnet_names[count.index], null)
   enforce_private_link_endpoint_network_policies = lookup(var.subnet_enforce_private_link_endpoint_network_policies, var.subnet_names[count.index], false)
