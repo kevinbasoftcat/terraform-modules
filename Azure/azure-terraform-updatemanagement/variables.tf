1eace053fd256d493ff388e529120534f780fbc6
kevinbasoftcat <kevinba@softcat.com>
2021-10-01 16:02:17 +0100

    Update to variables

--- Azure/azure-terraform-updatemanagement/variables.tf
+++ Azure/azure-terraform-updatemanagement/variables.tf
@@ -10,7 +10,7 @@ variable "resource_prefix" {
     type = string
     description = "Prefix to use when naming each of the resources please ensure this is only 3 characters long"
     validation {
-      condition = length (var.resourceprefix) == 3
+      condition = length (var.resource_prefix) == 3
       error_message = "The resource prefix must be only 3 characters long."
     }
 }
